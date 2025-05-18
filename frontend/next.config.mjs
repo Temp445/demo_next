96959a5232c5093bf9e7907613862250b26d2974
Temp445 <acetemp345@gmail.com>
2025-05-18 20:27:51 +0530

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: 'export',
+   images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
